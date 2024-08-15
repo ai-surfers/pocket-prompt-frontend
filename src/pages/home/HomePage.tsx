5da7919ab0f0e50a3f9589be57ffba8edc27fc80
Yooshin Kim <yusinkim.or@gmail.com>
2024-08-15 15:45:13 +0900

    test : 홈화면에 화면 이동 테스트코드 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,12 @@
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
-    return <div>HomePage</div>;
+    const navigate = useNavigate();
+    return (
+        <div>
+            <Button onClick={() => navigate("/price")}>결제 화면</Button>
+            <Button onClick={() => navigate("/my")}>구독 화면</Button>
+        </div>
+    );
 }
