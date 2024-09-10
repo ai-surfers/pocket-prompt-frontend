5187357dd6baa29923ebbe0a079fea522d51685d
Yooshin Kim <yusinkim.or@gmail.com>
2024-09-11 02:39:50 +0900

    chore : 기본 스타일 세팅

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export default function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderLeftContainer></HeaderLeftContainer>
+            <HeaderRightContainer></HeaderRightContainer>
+        </HeaderContainer>
+    );
+}
+
+const HeaderContainer = styled.header`
+    width: 100%;
+    background: #fff;
+    border: 1px solid black;
+
+    ${({ theme }) => theme.mixins.flexBox("row", "space-between")};
+    height: 52px;
+    padding: 0px 120px;
+
+    opacity: var(--sds-size-stroke-border);
+    background: rgba(255, 255, 255, 0.6);
+    backdrop-filter: blur(40px);
+
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+`;
+
+const HeaderLeftContainer = styled.div``;
+const HeaderRightContainer = styled.div``;
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header/Header";
+import styled from "styled-components";
 
 export default function Layout() {
     return (
-        <>
+        <LayoutContainer>
+            <Header />
             <Outlet />
-        </>
+        </LayoutContainer>
     );
 }
+
+const LayoutContainer = styled.div`
+    position: relative;
+`;
