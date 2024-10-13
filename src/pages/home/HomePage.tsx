e7f90edabe501174bddaafe727f555d0526a9313
Yooshin Kim <yusinkim.or@gmail.com>
2024-10-13 21:05:49 +0900

    [기타] Cloudflare 배포룰 세팅 (#11)
    * Revert "Revert "[기타] 헤더 수정 및 프리뷰 배포룰 작성 (#9)""
    
    This reverts commit c61e18b6683d180f81878a33d8989affd63860bd.
    
    * chore : submodule ssh로 변경
    
    * chore : nvmrc 추가
    
    * revert : github pages 관련 설정들 제거
    
    * chore : cloudflare preview 배포룰 설정
    
    * chore : cloudflare 토큰 변경
    
    * chore : permission 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,5 @@
 import { Wrapper } from "@/layouts/Layout";
-import PocketPromptPage from "../../components/home/PocketPromptPage";
 
 export default function HomePage() {
-    return (
-        <Wrapper>
-            <PocketPromptPage />
-        </Wrapper>
-    );
+    return <Wrapper>Home</Wrapper>;
 }
