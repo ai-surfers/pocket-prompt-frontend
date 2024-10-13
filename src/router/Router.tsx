a7781bdbcb626fbf7a145891e23d00cdefcc857d
Yooshin Kim <yusinkim.or@gmail.com>
2024-10-13 20:05:19 +0900

    Revert "Revert "[기타] 헤더 수정 및 프리뷰 배포룰 작성 (#9)""
    This reverts commit c61e18b6683d180f81878a33d8989affd63860bd.

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,26 +4,36 @@ import Layout from "@/layouts/Layout";
 import HomePage from "@/pages/home/HomePage";
 import PricePage from "@/pages/price/PricePage";
 import MyPage from "@/pages/my/MyPage";
+import ExtensionPage from "@/pages/extension/ExtensionPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Layout />,
+            children: [
+                {
+                    path: "/",
+                    element: <HomePage />,
+                },
+                {
+                    path: "/extension",
+                    element: <ExtensionPage />,
+                },
+                {
+                    path: "/price",
+                    element: <PricePage />,
+                },
+                {
+                    path: "/my",
+                    element: <MyPage />,
+                },
+            ],
+        },
+    ],
     {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                path: "/",
-                element: <HomePage />,
-            },
-            {
-                path: "/price",
-                element: <PricePage />,
-            },
-            {
-                path: "/my",
-                element: <MyPage />,
-            },
-        ],
-    },
-]);
+        basename: import.meta.env.BASE_URL,
+    }
+);
 
 export default router;
