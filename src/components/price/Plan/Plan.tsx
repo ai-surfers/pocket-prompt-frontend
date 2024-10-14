315040237ebae41ab57a5926fc1098f21c71c672
jiwoochris <travelandi01@gmail.com>
2024-10-15 00:31:01 +0900

    hotfix: user_tier to plan

--- src/components/price/Plan/Plan.tsx
+++ src/components/price/Plan/Plan.tsx
@@ -5,8 +5,8 @@ import { PLAN_DATA } from "./PlanData";
 import PlanCard from "./PlanCard";
 import PortOne from "@portone/browser-sdk/v2";
 import {
-    SubscriptionRequest,
-    usePostPayments,
+  SubscriptionRequest,
+  usePostPayments,
 } from "../../../hooks/mutations/payments/usePostPayments";
 import { useUser } from "@/hooks/useUser";
 const { Text } = Typography;
@@ -15,108 +15,103 @@ const PORTONE_STORE_ID = import.meta.env.VITE_PORTONE_STORE_ID;
 const PORTONE_CHANNEL_KEY = import.meta.env.VITE_PORTONE_CHANNEL_KEY;
 
 export default function Plan() {
-    const { userData } = useUser();
-    const [billingCycle, setBillingCycle] = useState("월간");
+  const { userData } = useUser();
+  const [billingCycle, setBillingCycle] = useState("월간");
 
-    const { mutate: subscription } = usePostPayments({
-        onSuccess(res) {
-            alert("정기결제가 등록되었습니다.");
-            console.log("usePostPayments - success", res);
-        },
-        onError(e) {
-            alert(e.message);
-            console.error("usePostPayments - error", e);
-        },
-    });
+  const { mutate: subscription } = usePostPayments({
+    onSuccess(res) {
+      alert("정기결제가 등록되었습니다.");
+      console.log("usePostPayments - success", res);
+    },
+    onError(e) {
+      alert(e.message);
+      console.error("usePostPayments - error", e);
+    },
+  });
 
-    const handleCycleChange = (value: string) => {
-        setBillingCycle(value);
-    };
+  const handleCycleChange = (value: string) => {
+    setBillingCycle(value);
+  };
 
-    const handleStartClick = (planType: string) => {
-        console.log(`선택된 요금제: ${planType}, 주기: ${billingCycle}`);
+  const handleStartClick = (planType: string) => {
+    console.log(`선택된 요금제: ${planType}, 주기: ${billingCycle}`);
 
-        if (!userData.isLogin) {
-            alert("로그인 후 이용 가능합니다.");
-            return;
-        }
-
-        if (planType === "free") {
-            console.log("무시");
-        } else {
-            console.log("결제!");
-            requestBillingKey(planType);
-        }
-    };
-
-    // [Reference] https://developers.portone.io/opi/ko/integration/start/v2/billing/issue?v=v2
-    async function requestBillingKey(planType: string) {
-        const issueResponse = await PortOne.requestIssueBillingKey({
-            storeId: PORTONE_STORE_ID,
-            channelKey: PORTONE_CHANNEL_KEY,
-            billingKeyMethod: "CARD",
-        });
-
-        console.log("issueResponse", issueResponse);
-        if (issueResponse?.code != null || !issueResponse?.billingKey) {
-            return alert(
-                issueResponse?.message || "빌링키 오류가 발생했습니다."
-            );
-        }
-
-        const request: SubscriptionRequest = {
-            billing_key: issueResponse?.billingKey,
-            payment_gateway: "tosspayments",
-            user_tier: planType,
-        };
-
-        subscription(request);
+    if (!userData.isLogin) {
+      alert("로그인 후 이용 가능합니다.");
+      return;
     }
 
-    const plans = useMemo(() => {
-        return billingCycle === "연간" ? PLAN_DATA.annual : PLAN_DATA.monthly;
-    }, [billingCycle]);
+    if (planType === "free") {
+      console.log("무시");
+    } else {
+      console.log("결제!");
+      requestBillingKey(planType);
+    }
+  };
 
-    return (
-        <PlanContainer>
-            <Segmented
-                onChange={handleCycleChange}
-                options={["월간", "연간"]}
+  // [Reference] https://developers.portone.io/opi/ko/integration/start/v2/billing/issue?v=v2
+  async function requestBillingKey(planType: string) {
+    const issueResponse = await PortOne.requestIssueBillingKey({
+      storeId: PORTONE_STORE_ID,
+      channelKey: PORTONE_CHANNEL_KEY,
+      billingKeyMethod: "CARD",
+    });
+
+    console.log("issueResponse", issueResponse);
+    if (issueResponse?.code != null || !issueResponse?.billingKey) {
+      return alert(issueResponse?.message || "빌링키 오류가 발생했습니다.");
+    }
+
+    const request: SubscriptionRequest = {
+      billing_key: issueResponse?.billingKey,
+      payment_gateway: "tosspayments",
+      user_plan: planType,
+    };
+
+    subscription(request);
+  }
+
+  const plans = useMemo(() => {
+    return billingCycle === "연간" ? PLAN_DATA.annual : PLAN_DATA.monthly;
+  }, [billingCycle]);
+
+  return (
+    <PlanContainer>
+      <Segmented onChange={handleCycleChange} options={["월간", "연간"]} />
+
+      <CardGrid>
+        {plans.map((plan) => (
+          <AnimatedCard key={plan.planType}>
+            <PlanCard
+              title={plan.title}
+              price={plan.price}
+              period={plan.period}
+              features={plan.features}
+              buttonLabel={plan.buttonLabel}
+              onClick={() => handleStartClick(plan.planType)}
             />
+          </AnimatedCard>
+        ))}
+      </CardGrid>
 
-            <CardGrid>
-                {plans.map((plan) => (
-                    <AnimatedCard key={plan.planType}>
-                        <PlanCard
-                            title={plan.title}
-                            price={plan.price}
-                            period={plan.period}
-                            features={plan.features}
-                            buttonLabel={plan.buttonLabel}
-                            onClick={() => handleStartClick(plan.planType)}
-                        />
-                    </AnimatedCard>
-                ))}
-            </CardGrid>
-
-            <Text>예산에 구애받지 마세요, 창의성을 발휘하세요!</Text>
-        </PlanContainer>
-    );
+      <Text>예산에 구애받지 마세요, 창의성을 발휘하세요!</Text>
+    </PlanContainer>
+  );
 }
 
 const PlanContainer = styled.section`
-    ${({ theme }) => theme.mixins.flexBox("column")};
-    gap: 20px;
-    margin: 40px 0;
+  ${({ theme }) => theme.mixins.flexBox("column")};
+  gap: 20px;
+  margin: 40px 0;
 `;
 
 const CardGrid = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 16px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 16px;
 `;
 
 const AnimatedCard = styled.div`
-    ${({ theme }) => theme.mixins.slideUpWFadeIn()};
+  ${({ theme }) => theme.mixins.slideUpWFadeIn()};
 `;
