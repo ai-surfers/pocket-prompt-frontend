7ade8186ef476b5e2a3ba48e18857f91165a52c4
Ginny <ginny@mineis.io>
2024-10-17 18:11:41 +0900

    add components on the extension page

---
+++ src/pages/extension/components/Extension7/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Extension7Container = styled.div`
+    position: relative;
+    width: 100%;
+    height: 630px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+export const Extension7BackgroundImage = styled.img`
+    position: absolute;
+    width: 100%;
+    height: 630px;
+    top: 0px;
+    left: 0px;
+    z-index: -1;
+`;
+
+export const Title = styled.p`
+    color: var(--white, #fff);
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 144%; /* 51.84px */
+    letter-spacing: -0.72px;
+    margin-bottom: 28px;
+`;
