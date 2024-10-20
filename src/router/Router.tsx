fbbb98e5130563a543e814b7ab86a7e29f39b2ad
Yooshin Kim <yusinkim.or@gmail.com>
2024-10-20 20:53:57 +0900

    fix : router re-import

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-
 import Layout from "@/layouts/Layout";
-import HomePage from "@/pages/Home";
-import PricePage from "@/pages/Price";
-import MyPage from "@/pages/My";
-import ExtensionPage from "@/pages/Extension";
-import PromptNewPage from "@/pages/PromptNew";
 import LayoutWithFooter from "@/layouts/LayoutWithFooter";
+import ExtensionPage from "@/pages/Extension";
+import HomePage from "@/pages/Home";
+import MyPage from "@/pages/My";
+import PricePage from "@/pages/Price";
+import PromptNewPage from "@/pages/PromptNew";
 
 const router = createBrowserRouter([
     {
