f1c6ba54987f2f876f91908e15d2483d751082a5
Yooshin Kim <yusinkim.or@gmail.com>
2024-10-20 23:01:32 +0900

    style : 배경색 변경

--- src/pages/promptNew/index.tsx
+++ src/pages/promptNew/index.tsx
@@ -26,141 +26,153 @@ export default function PromptNewPage() {
 
     return (
         <Container>
-            <Text font="large_32_bold" style={{ marginTop: "40px" }}>
-                나만의 프롬프트 등록하기
-            </Text>
+            <PromptNewWrapper>
+                <Text font="large_32_bold" style={{ marginTop: "40px" }}>
+                    나만의 프롬프트 등록하기
+                </Text>
 
-            <Text font="h2_20_reg" color="G_400">
-                실시간으로 미리보기 화면을 보면서 등록하는 나만의 프롬프트
-            </Text>
+                <Text font="h2_20_reg" color="G_400">
+                    실시간으로 미리보기 화면을 보면서 등록하는 나만의 프롬프트
+                </Text>
 
-            <Flex
-                justify="space-between"
-                align="stretch"
-                gap={16}
-                style={{ marginTop: "32px" }}
-            >
-                <Box flex="3">
-                    <Text font="h2_20_bold">내 프롬프트 미리보기</Text>
+                <Flex
+                    justify="space-between"
+                    align="stretch"
+                    gap={16}
+                    style={{ marginTop: "32px" }}
+                >
+                    <Box flex="3">
+                        <Text font="h2_20_bold">내 프롬프트 미리보기</Text>
 
-                    <Flex vertical gap={30} style={{ marginTop: "24px" }}>
-                        <Flex vertical gap={6}>
-                            <ExampleBox font="b3_14_reg" color="G_300">
-                                프롬프트 제목이 이곳에 표시됩니다.
-                            </ExampleBox>
-                            <ExampleBox font="b3_14_reg" color="G_300">
-                                프롬프트 설명이 이곳에 표시됩니다.
-                            </ExampleBox>
-                        </Flex>
-
-                        <ExampleBox
-                            height="532px"
-                            font="b3_14_reg"
-                            color="G_300"
-                        >
-                            프롬프트 내용에 따른 미리보기가 이곳에 표시됩니다.
-                        </ExampleBox>
-                    </Flex>
-                </Box>
-                <Box flex="7" border="primary_50">
-                    <Flex justify="space-between" align="center">
-                        <Text font="h2_20_bold">내 프롬프트 입력하기</Text>
-                        <Toggle
-                            items={["Public", "Private"]}
-                            value={visible}
-                            onChange={(val) => setVisible(val)}
-                        />
-                    </Flex>
-
-                    <Flex vertical gap={32} style={{ marginTop: "9px" }}>
-                        <FormItem title="프롬프트 제목" tags={["필수"]}>
-                            <Input
-                                placeholder="프롬프트의 제목을 입력해주세요."
-                                value={title}
-                                onChange={(val) => setTitle(val)}
-                                count={10}
-                            />
-                        </FormItem>
-
-                        <FormItem
-                            title="프롬프트 설명"
-                            tags={["필수"]}
-                            description=" 다른 사람들이 프롬프트를 더 쉽게 이해할 수 있도록 설명을 입력해주세요!"
-                        >
-                            <Textarea
-                                placeholder="예시: 주제와 청중을 입력하면 근사한 파워포인트 초안을 만들어주는 프롬프트"
-                                value={description}
-                                onChange={(val) => setDescription(val)}
-                                count={300}
-                            />
-                        </FormItem>
-
-                        <FormItem
-                            title="프롬프트 템플릿"
-                            tags={["필수"]}
-                            description=" [주제], [청중] 처럼 다른 사용자들에게 입력 받고 싶은 항목을 대괄호로 감싸주세요."
-                        >
-                            <Flex gap={4} justify="end">
-                                <Text font="b3_14_reg" color="primary_100">
-                                    어떻게 작성해야 할지 모르겠다면?
-                                </Text>
-                                <IcInfoCircle width={20} />
+                        <Flex vertical gap={30} style={{ marginTop: "24px" }}>
+                            <Flex vertical gap={6}>
+                                <ExampleBox font="b3_14_reg" color="G_300">
+                                    프롬프트 제목이 이곳에 표시됩니다.
+                                </ExampleBox>
+                                <ExampleBox font="b3_14_reg" color="G_300">
+                                    프롬프트 설명이 이곳에 표시됩니다.
+                                </ExampleBox>
                             </Flex>
 
-                            <Textarea
-                                placeholder="예시: [주제]를 주제로 한 파워포인트의 초안을 작성해줘.
-총 10 슬라이드로 이루어져있고, 청중은 [청중]을 대상으로 고려해줘."
-                                value={description}
-                                onChange={(val) => setDescription(val)}
-                            />
-                        </FormItem>
-
-                        <Flex style={{ width: "100%" }} gap={16}>
-                            <FormItem
-                                title="사용한 AI"
-                                tags={["필수", "복수 선택 가능"]}
-                                style={{ flex: 1 }}
+                            <ExampleBox
+                                height="532px"
+                                font="b3_14_reg"
+                                color="G_300"
                             >
-                                <Select
-                                    placeholder="사용한 AI를 선택해주세요."
-                                    style={{ width: "100%", marginTop: "8px" }}
-                                    mode="multiple"
-                                    options={AI}
-                                />
-                            </FormItem>
-
-                            <FormItem
-                                title="분야"
-                                tags={["필수", " 최대 5개 선택 가능"]}
-                                style={{ flex: 1 }}
-                            >
-                                <Select
-                                    placeholder="프롬프트의 분야를 선택해주세요."
-                                    style={{ width: "100%", marginTop: "8px" }}
-                                    options={CATEGORY}
-                                    mode="multiple"
-                                    maxCount={5}
-                                />
-                            </FormItem>
+                                프롬프트 내용에 따른 미리보기가 이곳에
+                                표시됩니다.
+                            </ExampleBox>
                         </Flex>
-                    </Flex>
+                    </Box>
+                    <Box flex="7" border="primary_50">
+                        <Flex justify="space-between" align="center">
+                            <Text font="h2_20_bold">내 프롬프트 입력하기</Text>
+                            <Toggle
+                                items={["Public", "Private"]}
+                                value={visible}
+                                onChange={(val) => setVisible(val)}
+                            />
+                        </Flex>
 
-                    <Button
-                        size={52}
-                        width="100%"
-                        style={{ marginTop: "60px" }}
-                    >
-                        프롬프트 등록 완료하기
-                    </Button>
-                </Box>
-            </Flex>
+                        <Flex vertical gap={32} style={{ marginTop: "9px" }}>
+                            <FormItem title="프롬프트 제목" tags={["필수"]}>
+                                <Input
+                                    placeholder="프롬프트의 제목을 입력해주세요."
+                                    value={title}
+                                    onChange={(val) => setTitle(val)}
+                                    count={10}
+                                />
+                            </FormItem>
+
+                            <FormItem
+                                title="프롬프트 설명"
+                                tags={["필수"]}
+                                description=" 다른 사람들이 프롬프트를 더 쉽게 이해할 수 있도록 설명을 입력해주세요!"
+                            >
+                                <Textarea
+                                    placeholder="예시: 주제와 청중을 입력하면 근사한 파워포인트 초안을 만들어주는 프롬프트"
+                                    value={description}
+                                    onChange={(val) => setDescription(val)}
+                                    count={300}
+                                />
+                            </FormItem>
+
+                            <FormItem
+                                title="프롬프트 템플릿"
+                                tags={["필수"]}
+                                description=" [주제], [청중] 처럼 다른 사용자들에게 입력 받고 싶은 항목을 대괄호로 감싸주세요."
+                            >
+                                <Flex gap={4} justify="end">
+                                    <Text font="b3_14_reg" color="primary_100">
+                                        어떻게 작성해야 할지 모르겠다면?
+                                    </Text>
+                                    <IcInfoCircle width={20} />
+                                </Flex>
+
+                                <Textarea
+                                    placeholder="예시: [주제]를 주제로 한 파워포인트의 초안을 작성해줘.
+총 10 슬라이드로 이루어져있고, 청중은 [청중]을 대상으로 고려해줘."
+                                    value={description}
+                                    onChange={(val) => setDescription(val)}
+                                />
+                            </FormItem>
+
+                            <Flex style={{ width: "100%" }} gap={16}>
+                                <FormItem
+                                    title="사용한 AI"
+                                    tags={["필수", "복수 선택 가능"]}
+                                    style={{ flex: 1 }}
+                                >
+                                    <Select
+                                        placeholder="사용한 AI를 선택해주세요."
+                                        style={{
+                                            width: "100%",
+                                            marginTop: "8px",
+                                        }}
+                                        mode="multiple"
+                                        options={AI}
+                                    />
+                                </FormItem>
+
+                                <FormItem
+                                    title="분야"
+                                    tags={["필수", " 최대 5개 선택 가능"]}
+                                    style={{ flex: 1 }}
+                                >
+                                    <Select
+                                        placeholder="프롬프트의 분야를 선택해주세요."
+                                        style={{
+                                            width: "100%",
+                                            marginTop: "8px",
+                                        }}
+                                        options={CATEGORY}
+                                        mode="multiple"
+                                        maxCount={5}
+                                    />
+                                </FormItem>
+                            </Flex>
+                        </Flex>
+
+                        <Button
+                            size={52}
+                            width="100%"
+                            style={{ marginTop: "60px" }}
+                        >
+                            프롬프트 등록 완료하기
+                        </Button>
+                    </Box>
+                </Flex>
+            </PromptNewWrapper>
         </Container>
     );
 }
 
-const Container = styled(Wrapper)`
-    max-width: 1080px;
+const Container = styled.div`
+    width: 100vw;
+    background: linear-gradient(180deg, #fff 0%, #f8f9fa 11.48%, #f7f8f9 100%);
+`;
 
+const PromptNewWrapper = styled(Wrapper)`
     padding-left: 40px;
     padding-right: 40px;
     padding-bottom: 40px;
