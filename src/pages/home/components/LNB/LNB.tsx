ef1c5b564ca636b355a39cf95230c3e83fb7a02f
hailey-hy <lillyine.hy@gmail.com>
2024-10-24 14:46:51 +0900

    fix: cloneElement 제거

--- src/pages/home/components/LNB/LNB.tsx
+++ src/pages/home/components/LNB/LNB.tsx
@@ -12,11 +12,11 @@ const LNB = () => {
     const [selectedKey, setSelectedKey] = useState<string>("1");
 
     const menuItems = [
-        { key: "1", label: "텍스트 프롬프트", icon: <TextSVG /> },
-        { key: "2", label: "이미지 프롬프트", icon: <Image /> },
-        { key: "3", label: "동영상 프롬프트", icon: <Video /> },
+        { key: "1", label: "텍스트 프롬프트", icon: TextSVG },
+        { key: "2", label: "이미지 프롬프트", icon: Image },
+        { key: "3", label: "동영상 프롬프트", icon: Video },
         { type: "divider", key: "divider-1" },
-        { key: "4", label: "저장한 프롬프트", icon: <BookMark /> },
+        { key: "4", label: "저장한 프롬프트", icon: BookMark },
     ];
 
     // 메뉴 항목을 동적으로 생성
@@ -27,13 +27,13 @@ const LNB = () => {
 
         return {
             key: item.key,
-            icon: (
+            icon: item.icon ? ( // icon이 있는 경우에만 MenuItemIcon 렌더링
                 <MenuItemIcon
                     menuKey={item.key}
                     icon={item.icon}
                     selectedKey={selectedKey}
                 />
-            ),
+            ) : null,
             label: item.label,
         };
     });
--- src/pages/home/components/LNB/MenuItemIcon/MenuItemIcon.tsx
+++ src/pages/home/components/LNB/MenuItemIcon/MenuItemIcon.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { SVGProps } from "react";
 import { useTheme } from "styled-components";
 
 interface MenuItemIconProps {
     menuKey: string;
-    icon: React.ReactNode;
+    icon: React.ComponentType<SVGProps<SVGSVGElement>>;
     selectedKey: string;
 }
 
-const MenuItemIcon = ({ menuKey, icon, selectedKey }: MenuItemIconProps) => {
+const MenuItemIcon = ({
+    menuKey,
+    icon: Icon,
+    selectedKey,
+}: MenuItemIconProps) => {
     const theme = useTheme();
 
     // 선택된 메뉴에 따라 아이콘의 stroke 및 fill을 변경
@@ -15,10 +19,7 @@ const MenuItemIcon = ({ menuKey, icon, selectedKey }: MenuItemIconProps) => {
         selectedKey === menuKey ? theme.colors.white : theme.colors.G_400;
     const fillColor = selectedKey === menuKey ? theme.colors.primary : "none";
 
-    return React.cloneElement(icon as React.ReactElement, {
-        stroke: strokeColor,
-        fill: fillColor,
-    });
+    return <Icon stroke={strokeColor} fill={fillColor} />;
 };
 
 export default MenuItemIcon;
