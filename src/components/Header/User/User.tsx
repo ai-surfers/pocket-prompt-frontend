c1d1f93d53b7a662c35e249691b95b63fd09b5bd
hailey-hy <lillyine.hy@gmail.com>
2024-12-30 17:13:33 +0900

    feat: 헤더 유저 버튼 hover, selected state 추가

--- src/components/Header/User/User.tsx
+++ src/components/Header/User/User.tsx
@@ -1,5 +1,5 @@
 import { useUser } from "@/hooks/useUser";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Text from "@/components/common/Text/Text";
 import Icon from "@/components/common/Icon";
@@ -8,29 +8,47 @@ export default function User() {
     const { userData } = useUser();
     const navigate = useNavigate();
 
+    const location = useLocation();
+    const isSelected = location.pathname.includes("my");
+
     return (
-        <Wrapper onClick={() => navigate("/my")}>
-            <UserWrapper>
-                <Text font="b3_14_reg" color="G_800">
+        <Wrapper $isSelected={isSelected}>
+            <UserWrapper onClick={() => navigate("/my")}>
+                <Text
+                    font="b3_14_reg"
+                    color={isSelected ? "primary_100" : "G_800"}
+                >
                     {userData.user?.nickname}
                 </Text>
-                <Icon name="User" color="G_800" />
+                <Icon
+                    name="User"
+                    color={isSelected ? "primary_100" : "G_800"}
+                />
             </UserWrapper>
         </Wrapper>
     );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ $isSelected: boolean }>`
     ${({ theme }) => theme.mixins.flexBox()};
     gap: 10px;
-
     cursor: pointer;
+
+    background-color: ${({ $isSelected }) =>
+        $isSelected ? "#F2F3FD" : "#f1f2f6"};
+    border-radius: 8px;
+    border: ${({ $isSelected }) =>
+        $isSelected && "1.5px solid var(--primary-50, #BBC0F5)"};
+
+    :hover {
+        background-color: ${({ $isSelected }) => !$isSelected && "#dee0e8"};
+        border-radius: 8px;
+    }
 `;
 
 const UserWrapper = styled.div`
     ${({ theme }) => theme.mixins.flexBox()};
+    cursor: pointer;
     padding: 8px 8px 8px 12px;
     gap: 10px;
-    background-color: #f1f2f6;
-    border-radius: 8px;
 `;
