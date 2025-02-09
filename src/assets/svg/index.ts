a752da72b4dafc4c336591eedca4e3be8d8b5bf8
hailey-hy <lillyine.hy@gmail.com>
2025-02-09 20:44:51 +0900

    feat: 리액트 파일 제거

--- src/assets/svg/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as IcInfoCircle } from "./IcInfoCircle";
-export { default as ImgEmpty } from "./ImgEmpty";
-export { default as Logo } from "./Logo";
-export { default as LogoNoLine } from "./LogoNoLine";
-export { default as ImgEmptyColor } from "./ImgEmptyColor";
--- src/core/UtmUri.ts
+++
@@ -1,5 +0,0 @@
-export const UTM_OVER_USAGE_LIMIT_URL =
-    "https://www.pocket-prompt.com/price?utm_source=product_web&utm_medium=notification&utm_campaign=pocket_run_credit_limit";
-
-export const UTM_TIER_LIMIT_URL =
-    "https://www.pocket-prompt.com/price?utm_source=product_web&utm_medium=notification&utm_campaign=pocket_run_premium_model";
