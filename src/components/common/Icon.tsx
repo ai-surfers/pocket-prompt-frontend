647e8a414820f637280a7312338442edace81201
hyo-ryeong <jhl9071@naver.com>
2025-02-26 08:32:59 +0900

    fix : icon type 확장

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,23 +1,37 @@
 "use client";
 
+import React from "react";
 import theme from "@/styles/theme";
 import * as Icons from "iconsax-react";
 
+export type IconNameType =
+    | keyof typeof Icons
+    | React.ComponentType<React.SVGProps<SVGSVGElement>>;
+
 interface IconProps {
-    name: keyof typeof Icons;
+    name: IconNameType;
     color?: keyof typeof theme.colors;
     size?: number;
     onClick?: () => void;
 }
 
-export default function Icon({ name, color, size, onClick }: IconProps) {
-    const Iconsax = Icons[name];
-    const hexColor = color ? theme.colors[color] : theme.colors.primary;
+export default function Icon({
+    name,
+    color = "primary",
+    size = 20,
+    onClick,
+}: IconProps) {
+    const hexColor = theme.colors[color];
+
+    // name이 문자열이면 iconsax-react의 해당 아이콘 컴포넌트를 사용하고,
+    // 그렇지 않으면 전달받은 React 컴포넌트를 사용합니다.
+    const IconComponent =
+        typeof name === "string" ? Icons[name as keyof typeof Icons] : name;
 
     return (
-        <Iconsax
+        <IconComponent
             color={hexColor}
-            size={size || 20}
+            size={size}
             onClick={onClick}
             style={{ pointerEvents: "none" }}
         />
