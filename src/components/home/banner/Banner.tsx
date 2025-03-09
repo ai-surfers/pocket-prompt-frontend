0d316c5cc38b9624f998a84163ae3ced408d2719
hailey-hy <lillyine.hy@gmail.com>
2025-03-09 14:56:31 +0900

    feat: 모바일 배너 이미지 추가

--- src/components/home/banner/Banner.tsx
+++ src/components/home/banner/Banner.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
-import AIBanner from "@img/banner-ai-prompt.png";
+import AIBanner from "@img/banner/banner-ai-prompt.png";
+import AIBannerMobile from "@img/banner/banner-ai-prompt-mobile.png";
 import styled from "styled-components";
 import {
     searchedCategoryState,
@@ -11,17 +12,23 @@ import useDeviceSize from "@/hooks/useDeviceSize";
 const Banner = () => {
     const searchedkeyword = useRecoilValue(searchedKeywordState);
     const searchedCategory = useRecoilValue(searchedCategoryState);
-    const { isUnderTablet } = useDeviceSize();
+    const { isUnderTablet, isMobile } = useDeviceSize();
 
     const isVisible = !searchedkeyword && searchedCategory === "total";
 
     return (
-        <Wrapper $isVisible={isVisible} $isUnderTablet={isUnderTablet}>
+        <Wrapper
+            $isVisible={isVisible}
+            $isUnderTablet={isUnderTablet}
+            $isMobile={isMobile}
+        >
             <Image
-                src={AIBanner}
+                src={isMobile ? AIBannerMobile : AIBanner}
                 alt="banner-ai-prompt"
-                width={808}
-                height={124}
+                width={isMobile ? 736 : 808}
+                height={isMobile ? 320 : 124}
+                layout="intrinsic"
+                style={{ borderRadius: "15px" }}
             />
         </Wrapper>
     );
@@ -29,11 +36,19 @@ const Banner = () => {
 
 export default Banner;
 
-const Wrapper = styled.div<{ $isVisible: boolean; $isUnderTablet: boolean }>`
+const Wrapper = styled.div<{
+    $isVisible: boolean;
+    $isUnderTablet: boolean;
+    $isMobile: boolean;
+}>`
     margin: 0 auto 40px auto;
     opacity: ${({ $isVisible }) => ($isVisible ? 1 : 0)};
-    max-height: ${({ $isVisible }) =>
-        $isVisible ? "124px" : "0px"}; /* 배너 높이 지정 */
+    max-height: ${({ $isVisible, $isMobile }) =>
+        $isVisible
+            ? $isMobile
+                ? "320px"
+                : "124px"
+            : "0px"}; /* 배너 높이 지정 */
     overflow: hidden;
     transition: opacity 0.5s ease-in-out, max-height 0.5s ease-in-out;
     padding: ${({ $isUnderTablet }) => ($isUnderTablet ? "0 10px" : "none")};
