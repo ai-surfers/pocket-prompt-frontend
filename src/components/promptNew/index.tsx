72e2f9c804221de695e6e8985b3524afa3da4f47
Jiwoo Chris Jung <travelandi01@gmail.com>
2025-06-22 00:56:11 -0700

    fix: use edited title and description

--- src/components/promptNew/index.tsx
+++ src/components/promptNew/index.tsx
@@ -67,11 +67,6 @@ export default function NewPromptClient({
 
     // LNB 탭으로 관리
     const [activeTab, setActiveTab] = useState("1");
-    const [promptTemplate, setPromptTemplate] = useState("");
-    const [selectedTitle, setSelectedTitle] = useState<string | null>(null);
-    const [selectedDescription, setSelectedDescription] = useState<
-        string | null
-    >(null);
 
     const { isUnderTablet } = useDeviceSize();
     const [isClient, setIsClient] = useState(false);
@@ -140,8 +135,6 @@ export default function NewPromptClient({
             // 나머지 상태도 초기화
             setImageFileList([]);
             setSampleMediaUrls([]);
-            setSelectedTitle(null);
-            setSelectedDescription(null);
         }
 
         setIsChangeModalOpen(false);
@@ -358,8 +351,8 @@ export default function NewPromptClient({
                 const promptData: CreatePromptRequest = {
                     ...input,
                     type: typeMap[contentBy as keyof typeof typeMap],
-                    title: selectedTitle || input.title,
-                    description: selectedDescription || input.description,
+                    title: input.title,
+                    description: input.description,
                     visibility: input.visibility.toLowerCase(),
                     user_input_format: user_input_formats,
                     categories: input.categories || [],
@@ -488,7 +481,6 @@ export default function NewPromptClient({
             }
         }
 
-        if (templateValue) setPromptTemplate(templateValue);
         if (activeTab === "1") setActiveTab("2");
         else if (activeTab === "2") setActiveTab("3");
     };
@@ -625,9 +617,6 @@ export default function NewPromptClient({
                             <FormSecSection
                                 isEdit={isEdit}
                                 goToNextTab={goToNextTab}
-                                promptTemplate={promptTemplate}
-                                setSelectedTitle={setSelectedTitle}
-                                setSelectedDescription={setSelectedDescription}
                             />
                             {contentBy === "이미지 프롬프트" && (
                                 <ImgUploadSection
